4e71c8e809e7ff21a745dfb60c6aeac4dac6da0a
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-10-06 14:36:55 +0700

    Update Commit

--- config.php
+++ config.php
@@ -20,13 +20,17 @@ define('STR_ALERT_INBOX_CAPTION2', "Extend time to enjoy the benefits even longe
 define('STR_ALERT_INBOX_TITLE3', "SUBSCRIPTION HAD ENDED!");
 define('STR_ALERT_INBOX_CAPTION3', "Let's get another!");
 define('STR_ALERT_INBOX_TITLE4', "FREE CASH!");
-define('STR_ALERT_INBOX_CAPTION4', "Let's build a business!");
+define('STR_ALERT_INBOX_CAPTION4', "Let's build the other business!");
+define('STR_ALERT_INBOX_TITLE_WEST', "FREE CASH!");
+define('STR_ALERT_INBOX_CAPTION_WEST', "Let's build a business!");
+define('STR_ALERT_INBOX_TITLE_EAST', "CASH BONUS!");
+define('STR_ALERT_INBOX_CAPTION_EAST', "Continue your business journey now!");
 
 // REFERRAL REWARD
 
 $referral_reward = array(
-    "1" => "0.06,CASH", // reward = 60% from cash user
-    "2" => "50,CRYSTAL", // reward = 50 crystal
-    "3" => "0.06,CASH", // reward = 60% from cash user
-    "4" => "50,CRYSTAL", // reward = 50 crystal
+    "1" => "0.06,CASH,REFERRAL_1", // reward = 6% from networth user
+    "2" => "50,CRYSTAL,REFERRAL_2", // reward = 50 crystal
+    "3" => "0.06,CASH,REFERRAL_3", // reward = 6% from networth user
+    "4" => "50,CRYSTAL,REFERRAL_4", // reward = 50 crystal
 );

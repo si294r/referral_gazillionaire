3d256b78dba9982bbf2da64a67ad61fe5bfa92e7
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-10-06 18:24:59 +0700

    Update Commit

--- config.php
+++ config.php
@@ -29,8 +29,8 @@ define('STR_ALERT_INBOX_CAPTION_EAST', "Continue your business journey now!");
 // REFERRAL REWARD
 
 $referral_reward = array(
-    "1" => "0.06,CASH,REFERRAL_1", // reward = 6% from networth user
-    "2" => "50,CRYSTAL,REFERRAL_2", // reward = 50 crystal
-    "3" => "0.06,CASH,REFERRAL_3", // reward = 6% from networth user
-    "4" => "50,CRYSTAL,REFERRAL_4", // reward = 50 crystal
+    "1" => "0.06,CASH,INVITE_REWARD_1", // reward = 6% from networth user
+    "2" => "50,CRYSTAL,INVITE_REWARD_2", // reward = 50 crystal
+    "3" => "0.06,CASH,INVITE_REWARD_3", // reward = 6% from networth user
+    "4" => "50,CRYSTAL,INVITE_REWARD_4", // reward = 50 crystal
 );
